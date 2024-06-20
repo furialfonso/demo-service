d91eb18b108c04ca0b8dbb200fb2cf842240049e
ANDALFONSO <andresfelipe.alfonso@mercadolibre.com.co>
2024-06-20 17:56:07 -0500

    first commit

---
+++ src/main/java/com/example/demo/controllers/Ping.java
@@ -0,0 +1,15 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class Ping {
+
+  @GetMapping("/ping")
+  public String getMethodName() {
+    return "BB";
+  }
+
+}
